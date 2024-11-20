d9ae16cb8b6310341bf9dc906ca40c6796621139
bezhan5lion <bezhanomari.bo@gmail.com>
2024-11-20 16:45:32 -0500

    firstCommit

---
+++ src/test/java/SolarSystem.java
@@ -0,0 +1,16 @@
+import javax.sound.midi.Soundbank;
+
+public class SolarSystem {
+
+    public static void main(String[] args) {
+        BasicStructure BB = new BasicStructure();
+
+        BB.Moon="2";
+        BB.Planet="2";
+        BB.Stars="10";
+        BB.Sun="1";
+
+        System.out.println("Solar is ready");
+    }
+
+}
